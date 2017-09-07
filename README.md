a17a9f3f81db87b080dba5d7c1cc1f1fa2ac9f83
vladimir-tsvetkov <npacemo@gmail.com>
2017-09-07 18:04:04 +0300

    basic installation and instructions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+Clone the `dotfiles` Git repository and install them:
+
+    cd ~
+    git clone git@github.com:vladimir-tsvetkov/dotfiles.git
+    ~/dotfiles/install.sh
+
+
--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 
 export DOTFILES="~/dotfiles"
 
+# initialize Git submodules
+
+cd $DOTFILES
+git submodule init
+git submodule update
+cd ~
+
 # initialize the symlinks
 
 ln -sfv "$DOTFILES/runcom/.bash_profile" ~
