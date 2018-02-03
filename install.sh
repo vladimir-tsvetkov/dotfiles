2da4a3b210a5255650441008d305343989bebed7
vladimir-tsvetkov <npacemo@gmail.com>
2018-02-03 17:50:58 +0200

    add missing instruction to install.sh

--- install.sh
+++ install.sh
@@ -117,8 +117,12 @@ bash-it enable completion nvm
 brew install --HEAD coursier/formulas/coursier
 
 # install scalafmt:
-brew install --HEAD olafurpg/scalafmt/scalafmt
+# brew install --HEAD olafurpg/scalafmt/scalafmt
 brew install nailgun
+
+coursier bootstrap --standalone com.geirsson:scalafmt-cli_2.12:1.4.0 \
+  -r bintray:scalameta/maven \
+  -o /usr/local/bin/scalafmt_ng -f --main com.martiansoftware.nailgun.NGServer
 
 # initialize the symlinks
 ln -sfv "$DOTFILES/runcom/.bash_profile" ~
